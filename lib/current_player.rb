d216591d903eaea17eda8132cdd7f1db897d352d
Przemyslaw Dabrowski <bluh@outlook.com>
2018-07-18 22:05:38 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,10 @@
-counter = 0
+
 def turn_count(board)
+  counter = 0
   board.each do |space|
     if space != " " && space != ""
       counter += 1
     end
   end
-  counter
+  return counter
 end
